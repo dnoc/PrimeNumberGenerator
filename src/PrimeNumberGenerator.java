33515e3a594865f4b9b8c73c4ec2ea5590b2d648
dnoc <drew.noct@gmail.com>
2016-08-31 18:03:48 -0400

    performance: isPrime checks odds and a smaller range of factors

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -7,7 +7,7 @@ public class PrimeNumberGenerator {
         if (value < 2) return false;
         if (value == 2) return true;
         if (value % 2 == 0) return false;
-        for (int i = 3; i < value; i++) {
+        for (int i = 3; i * i <= value; i += 2) {
             if (value % i == 0) return false;
         }
         return true;
