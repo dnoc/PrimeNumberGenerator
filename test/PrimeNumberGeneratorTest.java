4613d20f286bad9d84138cbc52d1ab152f2e2fae
dnoc <drew.noct@gmail.com>
2016-08-31 19:15:13 -0400

    first 26 primes instead of first 25

--- test/PrimeNumberGeneratorTest.java
+++ test/PrimeNumberGeneratorTest.java
@@ -50,10 +50,10 @@ public class PrimeNumberGeneratorTest {
     }
 
     @Test
-    public void whenOneThroughHundredIsEnteredItReturnsPrimes() {
+    public void whenOneThrough101IsEnteredItReturnsPrimes() {
         List<Integer> output = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
-                47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
-        assertEquals(output, primeNumberGenerator.generate(1, 100));
+                47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101);
+        assertEquals(output, primeNumberGenerator.generate(1, 101));
     }
 
     @Test
@@ -77,7 +77,7 @@ public class PrimeNumberGeneratorTest {
     @Test
     public void whenOneThroughHundredIsEnteredInReverseOrderItReturnsPrimes() {
         List<Integer> output = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
-                47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97);
-        assertEquals(output, primeNumberGenerator.generate(100, 1));
+                47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101);
+        assertEquals(output, primeNumberGenerator.generate(101, 1));
     }
 }
