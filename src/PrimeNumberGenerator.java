9d14c29ae11ad6aee8ad3f7f971e67149045b7a1
dnoc <drew.noct@gmail.com>
2016-08-31 19:39:10 -0400

    PrimeGenerator now implements interface PrimeNumberGenerator

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -1,36 +1,6 @@
-import java.util.ArrayList;
 import java.util.List;
 
-public class PrimeNumberGenerator {
-
-    private boolean isPrime(int value) {
-        if (value < 2) return false;
-        if (value == 2) return true;
-        if (value % 2 == 0) return false;
-        for (int i = 3; i <= Math.sqrt(value); i += 2) {
-            if (value % i == 0) return false;
-        }
-        return true;
-    }
-
-    public List<Integer> generate(int startingValue, int endingValue) {
-        List<Integer> primeList = new ArrayList<>();
-        int start, end;
-
-        if (startingValue > endingValue) {
-            start = endingValue;
-            end = startingValue;
-        } else {
-            start = startingValue;
-            end = endingValue;
-        }
-
-        for (int i = start; i <= end; i++) {
-            if (isPrime(i)) primeList.add(i);
-            if (i == Integer.MAX_VALUE) break;
-        }
-
-        return primeList;
-    }
-
+public interface PrimeNumberGenerator {
+    List<Integer> generate(int startingValue, int endingValue);
+    boolean isPrime(int value);
 }
--- test/PrimeNumberGeneratorTest.java
+++ test/PrimeNumberGeneratorTest.java
@@ -6,78 +6,78 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class PrimeNumberGeneratorTest {
-    PrimeNumberGenerator primeNumberGenerator;
+    PrimeNumberGenerator primeGenerator;
 
     @Before
     public void setup() {
-        primeNumberGenerator = new PrimeNumberGenerator();
+        primeGenerator = new PrimeGenerator();
     }
 
     @Test
     public void whenLessThanTwoIsEnteredItReturnsNothing() {
         List<Integer> output = new ArrayList<>();
-        assertEquals(output, primeNumberGenerator.generate(0, 1));
+        assertEquals(output, primeGenerator.generate(0, 1));
     }
 
     @Test
     public void whenANegativeIsEnteredItReturnsNothing() {
         List<Integer> output = new ArrayList<>();
-        assertEquals(output, primeNumberGenerator.generate(-3, -3));
+        assertEquals(output, primeGenerator.generate(-3, -3));
     }
 
     @Test
     public void whenANegativeRangeIsEnteredItReturnsNoNegatives() {
         List<Integer> output = Arrays.asList(2, 3);
-        assertEquals(output, primeNumberGenerator.generate(-5, 3));
+        assertEquals(output, primeGenerator.generate(-5, 3));
     }
 
     @Test
     public void whenTwoIsEnteredItReturnsTwo() {
         List<Integer> output = Arrays.asList(2);
-        assertEquals(output, primeNumberGenerator.generate(2, 2));
+        assertEquals(output, primeGenerator.generate(2, 2));
     }
 
     @Test
     public void whenFourIsEnteredItReturnsNothing() {
         List<Integer> output = new ArrayList<>();
-        assertEquals(output, primeNumberGenerator.generate(4, 4));
+        assertEquals(output, primeGenerator.generate(4, 4));
     }
 
     @Test
     public void whenTwoThroughTenIsEnteredItReturnsPrimes() {
         List<Integer> output = Arrays.asList(2, 3, 5, 7);
-        assertEquals(output, primeNumberGenerator.generate(2, 10));
+        assertEquals(output, primeGenerator.generate(2, 10));
     }
 
     @Test
     public void whenOneThrough101IsEnteredItReturnsPrimes() {
         List<Integer> output = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
                 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101);
-        assertEquals(output, primeNumberGenerator.generate(1, 101));
+        assertEquals(output, primeGenerator.generate(1, 101));
     }
 
     @Test
     public void whenMaxValueIsEnteredItReturnsMaxValue() {
         List<Integer> output = Arrays.asList(Integer.MAX_VALUE);
-        assertEquals(output, primeNumberGenerator.generate(Integer.MAX_VALUE, Integer.MAX_VALUE));
+        assertEquals(output, primeGenerator.generate(Integer.MAX_VALUE, Integer.MAX_VALUE));
     }
 
     @Test
     public void when7900Through7920IsEnteredItReturnsPrimes() {
         List<Integer> output = Arrays.asList(7901, 7907, 7919);
-        assertEquals(output, primeNumberGenerator.generate(7900, 7919));
+        assertEquals(output, primeGenerator.generate(7900, 7919));
     }
 
     @Test
     public void whenTwoThroughTenIsEnteredInReverseOrderItReturnsPrimes() {
         List<Integer> output = Arrays.asList(2, 3, 5, 7);
-        assertEquals(output, primeNumberGenerator.generate(10, 2));
+        assertEquals(output, primeGenerator.generate(10, 2));
     }
 
     @Test
     public void whenOneThroughHundredIsEnteredInReverseOrderItReturnsPrimes() {
         List<Integer> output = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
                 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101);
-        assertEquals(output, primeNumberGenerator.generate(101, 1));
+        assertEquals(output, primeGenerator.generate(101, 1));
     }
 }
