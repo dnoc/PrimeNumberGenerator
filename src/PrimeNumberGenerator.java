bb2f15b796b303312e4d259217574f222c3125d1
dnoc <drew.noct@gmail.com>
2016-08-31 17:45:50 -0400

    Nonprime test done

--- src/PrimeNumberGenerator.java
+++ src/PrimeNumberGenerator.java
@@ -4,12 +4,16 @@ import java.util.List;
 public class PrimeNumberGenerator {
 
     private boolean isPrime(int value) {
+        if (value == 2) return true;
         return false;
     }
 
     public List<Integer> generate(int startingValue, int endingValue) {
         List<Integer> primeList = new ArrayList<>();
-        primeList.add(2);
+
+        if (isPrime(startingValue)) {
+            primeList.add(startingValue);
+        }
 
         return primeList;
     }
